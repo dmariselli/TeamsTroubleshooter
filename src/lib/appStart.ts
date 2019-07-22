e908bb22446f05325867a191713cdec91a69cfd7
junDrone <arjunsundaresh93@gmail.com>
2019-07-22 14:42:44 -0700

    Adding nprocess class

--- src/lib/appStart.ts
+++ src/lib/appStart.ts
@@ -2,18 +2,21 @@ import * as Clusterize from "clusterize.js";
 import * as eventStream from "event-stream";
 import * as fs from "fs";
 import { LogLine } from "./logLine";
+import { Process } from "./process";
 
 export function start(file: string) {
 
     // tslint:disable-next-line: max-line-length
     const regex = /^(\w{3} \w{3} \d{2} \d{4} \d{2}:\d{2}:\d{2} GMT[+-]\d{4}) \([A-Za-z .]+\) <(\d+)> -- (\w+) -- (.+)/;
     let lineCount = 0;
-    const logLines: LogLine[] = [];
+    let logLines: LogLine[] = [];
     const errors: string[] = [];
+    let map:Map<string, Process> = new Map<string, Process>();
 
     // tslint:disable-next-line: no-console
     console.time("Log Parsing");
-
+    let currentPid:string = undefined;
+    let currentClientVersion: string = undefined;
     fs.createReadStream(file)
         .pipe(eventStream.split())
         .pipe(
@@ -28,6 +31,18 @@ export function start(file: string) {
                         result[4],
                         lineCount,
                     );
+                    if(!currentPid) { currentPid = result[2];}
+                    else if(currentPid != result[2]) {
+                        map.set(currentPid, new Process(currentPid,logLines, currentClientVersion));
+                        logLines = [];
+                        currentPid = result[2];
+                        currentClientVersion = undefined;
+                    } else {
+                        let index = result[4].indexOf('Client version is:');
+                        if(index>0){
+                            currentClientVersion = result[4].substr(index);
+                        }
+                    }
                     logLines.push(logLine);
                 } catch (error) {
                     logLines.length > 0 ? logLines[logLines.length - 1].appendToMessage(line) : errors.push(line);
@@ -41,6 +56,8 @@ export function start(file: string) {
                 // tslint:disable-next-line: no-console
                 console.timeEnd("Log Parsing");
                 // tslint:disable-next-line: no-console
+                map.set(currentPid, new Process(currentPid,logLines,currentClientVersion));
+                console.log(map);
                 console.log(lineCount);
                 // tslint:disable-next-line: no-console
                 console.log(logLines);
